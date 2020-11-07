3549e93f58a731b333cec1288d403919bc0c5fde
EunsilSon <eunsil1023@gmail.com>
2020-11-08 02:10:21 +0900

    main-task/#3/201108

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.net.Socket;
+
+public class ClientHandler {
+    Socket clientSocket;
+
+    ServerRecv serverRecv;
+    //ServerSend serverSend;
+
+    public void streamSetting() {
+        try{
+            clientSocket.getInetAddress();
+            serverRecv = new ServerRecv(clientSocket);
+            //serverSend = new ServerSend(clientSocket);
+            new Thread(serverRecv).start();
+            //new Thread(serverSend).start();
+        }
+        catch(Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public ClientHandler(Socket clientSocket){
+            this.clientSocket = clientSocket;
+            streamSetting();
+    }
+}
